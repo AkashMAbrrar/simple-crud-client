88261d1abf26d6209c241a2390ae3c2c42671dad
AkashMAbrrar <madborakash48@gmail.com>
2024-11-25 15:34:03 +0600

    this commit for a post from server and data fetch from client is sucess

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
 import "./App.css";
 
 function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
   return (
     <>
       <h2>Simple Crud Client</h2>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" placeholder="Your Name" id="" />
+        <br />
+        <input type="email" placeholder="Your Email" name="email" id="" />
+        <br />
+        <input type="submit" value="Add User" />
+      </form>
     </>
   );
 }
